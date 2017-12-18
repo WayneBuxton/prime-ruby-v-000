a2a4f712a3c62689a989434562db3e74fc81c549
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-18 03:11:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,4 @@
 require 'pry'
 def prime?(number)
-  prime_array = []
-  p = 2
-  if number < 2
-    return true
-  end
-  while (number % p == 0) && (p < number)
-    prime_array.push(p)
-    p += 1
-  end
-  return true
+
 end
